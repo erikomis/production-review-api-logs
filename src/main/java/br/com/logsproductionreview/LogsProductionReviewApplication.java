33d9406a90cd51898a2f5b2caea6993da9e214d4
erik-martins <erik.ozzetti.martins@gmail.com>
2024-09-20 21:19:55 -0300

    intial

---
+++ src/main/java/br/com/logsproductionreview/LogsProductionReviewApplication.java
@@ -0,0 +1,13 @@
+package br.com.logsproductionreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsProductionReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogsProductionReviewApplication.class, args);
+    }
+
+}
