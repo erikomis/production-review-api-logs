bad2f479a1dcf9834665675823a787aaa0f5d8be
erik-martins <erik.ozzetti.martins@gmail.com>
2024-09-20 21:19:34 -0300

    feat: interface LogNotificationService

---
+++ src/main/java/br/com/logsproductionreview/service/LogNotificationService.java
@@ -0,0 +1,8 @@
+package br.com.logsproductionreview.service;
+
+import br.com.logsproductionreview.dto.LogNotificationDto;
+
+public interface LogNotificationService {
+
+    void  saveLogNotification(LogNotificationDto logNotificationDto);
+}
